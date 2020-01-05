fbc50fe082bc8ccc3f7f775889f915a81d3bb892
Arthamu <artha.mukherjee@gmail.com>
2020-01-06 03:33:47 +0530

    fixed build issue

--- Angular-UI/src/app/calender/calender.module.ts
+++ Angular-UI/src/app/calender/calender.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CalenderRoutingModule } from './calender-routing.module';
 import { CalenderComponent } from './calender.component';
-import { AppModule } from 'bin/src/app/app.module';
+
 import { AngularDraggableModule } from 'angular2-draggable';
 
 
